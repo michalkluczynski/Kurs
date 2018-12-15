010b58a8435023faa4ff2892d7c9c6427f786c10
michalkluczynski <michalkluczynski93@wp.pl>
2018-12-15 09:23:37 +0100

    Suma

---
+++ html/1_Java Script/skrypty/1_zmienne.js
@@ -0,0 +1,20 @@
+  //var - zmienna globalna
+  //Let - zmienna lokalna
+  var imie = "Janusz"
+  var nazwisko;
+  nazwisko = "Nowak";
+  document.write("twoje imie: ")
+
+  var wiek = 21;
+  document.write("<br>Twój wiek: " + wiek); // + konkatenacja - połączenie stringów (ciągu znaków)
+
+  console.log(wiek);
+
+  var x, y;
+  x = 10;
+  y = 7;
+  var suma = x + y;
+  // document.write(" <br> suma wynosi: " + (x + y));
+  // document.write(" <br> suma wynosi: " + suma);
+
+  alert(suma);
