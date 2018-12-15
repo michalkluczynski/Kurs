6f11f7579867625d21989ee501a51e40ff3c3d5b
michalkluczynski <michalkluczynski93@wp.pl>
2018-12-15 11:02:42 +0100

    zadanie

--- html/1_Java Script/skrypty/1_zmienne.js
+++ html/1_Java Script/skrypty/1_zmienne.js
@@ -45,3 +45,30 @@
   console.log(typeof(nic)); //object
 
   console.log(a +(a % 3) - 10);
+
+  var dziesietny = 11; //liczba w systemie dziesiętnym
+  var oktalny = 010; //liczba w systebie oktalnym
+  var szesnastkowy = 0x10 //liczba w systemie szesnastkowym
+
+  var liczba1 = prompt("Podaj liczbę: ");
+  console.log(liczba1);
+
+  // var liczba2 = prompt("Podaj drugą liczbę: ");
+  // console.log(liczba2);
+
+  //zamiana zmiennych na typ liczbowy
+  liczba1 = parseFloat(liczba1);
+  // liczba2 = parseFloat(liczba2);
+  //koniec zamiany
+
+  // var suma = liczba1 + liczba2;
+  // console.log("Suma wynosi: " + liczba1 + "+" + liczba2 + "=" + suma);
+
+  var potega = liczba1 * liczba1;
+  console.log("Potęga wybranej liczby wynosi: " + potega);
+
+  // Zadanie domowe - napisz program który obliczy pole prostokąta dane podaje użytkownik z klawiatury
+
+  // wynik wyświetlany: Bok a = ...... Bok b = ..... Pole = ...... cm^2 w indeksie górnym
+  
+  // zad 2 - podobnie trójkąt i koło
